a74b9d5bfd9e4ac160e50eb69d4e16640ec6a69a
ayush-glorywebs <55269750+ghetiaayush@users.noreply.github.com>
2022-05-16 14:53:26 +0530

    First commit

---
+++ resources/views/email_template.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>laravel sending email using smtp </title>
+</head>
+<body>
+<p>Hello sir/ma'am, <br/>I am {{ $emails['name'] }}</p>
+<p>following is my introduction:</p>
+<p>{{ $emails['message'] }}.</p>
+<!-- <p>It will be helpful if you give me response soon.Thank you.</p> -->
+</body>
+</html>
